4f3f21eb82242337bef169a48aa4d9b0a5e7ec9d
jjungyeun <wjyddd@naver.com>
2022-12-20 17:10:38 +0900

    [chore] CORS 설정 추가
    - 모든 origin을 허용하도록 설정 추가

--- test_platform/__init__.py
+++ test_platform/__init__.py
@@ -1,8 +1,11 @@
 from pydantic import BaseSettings
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 
 class Settings(BaseSettings):
+    origins = ["*"]
+
     class Rdb:
         scheme: str = "postgresql://"
         id: str = "testuser"
@@ -18,6 +21,14 @@ def initialize():
     Base.metadata.create_all(bind=engine)
     # Drop Tables
     Base.metadata.create_all(bind=engine)
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=settings.origins,
+        allow_credentials=True,     # cookie 포함 여부를 설정한다. 기본은 False
+        allow_methods=["*"],        # 허용할 method를 설정할 수 있으며, 기본값은 'GET'이다.
+        allow_headers=["*"],        # 허용할 http header 목록을 설정할 수 있으며 Content-Type, Accept, Accept-Language, Content-Language은 항상 허용된다.
+    )
 
 
 settings = Settings()
